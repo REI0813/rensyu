faff7a9a9af606097bdd69c04a0936d99a984afc
REI0813 <shrei.813315@gmail.com>
2026-07-29 22:57:32 +0900

    ListSample作成

--- src/com/github/rei0813/collection/MapSample.java
+++ src/com/github/rei0813/collection/MapSample.java
@@ -9,10 +9,10 @@ public class MapSample {
 	Map<String, String> map = new HashMap<String, String>( );
 	
 		map.put("key1", "value1");
-		map.put("key1", "value2");
-		map.put("key1", "value3");
-		map.put("key1", "value4");
-		map.put("key1", "value5");
+		map.put("key2", "value2");
+		map.put("key3", "value3");
+		map.put("key4", "value4");
+		map.put("key5", "value5");
 	
 		String value = map.get("key1"); 
 		System.out.println(value);
